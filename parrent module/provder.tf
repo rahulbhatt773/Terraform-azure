1b72b8c234ba2c123ee414057dd2393ba07ad3c7
Rahul Bhatt <rahulbhatt773@gmail.com>
2026-08-25 14:04:13 +0530

    infra5

--- parrent module/provder.tf
+++ parrent module/provder.tf
@@ -5,12 +5,6 @@ terraform {
       version = "4.81.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "rahul_rg"
-    storage_account_name = "rahulsto"
-    container_name       = "rahulblob"
-    key                  = "rahul.tfstate"
-  }
 }
 
 provider "azurerm" {
